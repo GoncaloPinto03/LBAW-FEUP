f1590c7317ea0e443f23b6380282c2e245345a69
GonASSalo LAsias Possacos dos Santos <up202108839@fe.up.pt>
2023-10-21 15:44:22 +0000

    Update populate_db.sql

--- sql/populate_db.sql
+++ sql/populate_db.sql
@@ -1,10 +1,11 @@
 INSERT INTO admin (name, email, password) 
 VALUES
-    ('Admin1', 'admin1@example.com', 'password1'),
-    ('Admin2', 'admin2@example.com', 'password2'),
-    ('Admin3', 'admin3@example.com', 'password3'),
-    ('Admin4', 'admin4@example.com', 'password4'),
-    ('Admin5', 'admin5@example.com', 'password5');
+    ('John Doe', 'johndoe@example.com', 'adminpass1'),
+    ('Jane Smith', 'janesmith@example.com', 'adminpass2'),
+    ('Michael Johnson', 'michaeljohnson@example.com', 'adminpass3'),
+    ('Emily Wilson', 'emilywilson@example.com', 'adminpass4'),
+    ('David Brown', 'davidbrown@example.com', 'adminpass5');
+
 
 
 INSERT INTO ban (user_id, admin_id) 
@@ -13,32 +14,34 @@ VALUES
     (2, 2);
 
 
+
 INSERT INTO users (email, name, password, reputation)
 VALUES
-    ('user1@example.com', 'User1', 'password1', 100),
-    ('user2@example.com', 'User2', 'password2', 150),
-    ('user3@example.com', 'User3', 'password3', 200),
-    ('user4@example.com', 'User4', 'password4', 50),
-    ('user5@example.com', 'User5', 'password5', 300),
-    ('user6@example.com', 'User6', 'password6', 75),
-    ('user7@example.com', 'User7', 'password7', 250),
-    ('user8@example.com', 'User8', 'password8', 30),
-    ('user9@example.com', 'User9', 'password9', 180),
-    ('user10@example.com', 'User10', 'password10', 90);
+    ('alice@example.com', 'Alice Johnson', 'password1', 100),
+    ('bob@example.com', 'Bob Smith', 'password2', 150),
+    ('charlie@example.com', 'Charlie Davis', 'password3', 200),
+    ('david@example.com', 'David Wilson', 'password4', 50),
+    ('eva@example.com', 'Eva Martin', 'password5', 300),
+    ('frank@example.com', 'Frank Harris', 'password6', 75),
+    ('grace@example.com', 'Grace Lee', 'password7', 250),
+    ('helen@example.com', 'Helen White', 'password8', 30),
+    ('ian@example.com', 'Ian Clark', 'password9', 180),
+    ('judy@example.com', 'Judy Brown', 'password10', 90);
+
 
 
 INSERT INTO comment (text, date, likes, dislikes, user_id, article_id)
 VALUES
-    ('This is a comment 1', CURRENT_TIMESTAMP, 10, 2, 1, 1),
-    ('This is a comment 2', CURRENT_TIMESTAMP, 5, 1, 2, 1),
-    ('This is a comment 3', CURRENT_TIMESTAMP, 12, 0, 3, 2),
-    ('This is a comment 4', CURRENT_TIMESTAMP, 8, 3, 4, 2),
-    ('This is a comment 5', CURRENT_TIMESTAMP, 15, 1, 5, 3),
-    ('This is a comment 6', CURRENT_TIMESTAMP, 3, 0, 6, 4),
-    ('This is a comment 7', CURRENT_TIMESTAMP, 7, 4, 7, 5),
-    ('This is a comment 8', CURRENT_TIMESTAMP, 2, 0, 8, 5),
-    ('This is a comment 9', CURRENT_TIMESTAMP, 6, 1, 9, 6),
-    ('This is a comment 10',CURRENT_TIMESTAMP, 4, 2, 10, 6);
+    ('Great article!', CURRENT_TIMESTAMP, 10, 2, 1, 1),
+    ('I found this very interesting', CURRENT_TIMESTAMP, 5, 1, 2, 1),
+    ('Good review!', CURRENT_TIMESTAMP, 8, 3, 4, 2),
+    ('Sports are awesome!', CURRENT_TIMESTAMP, 12, 0, 3, 3),
+    ('I completely agree with the author.', CURRENT_TIMESTAMP, 3, 0, 6, 4),
+    ('I want to visit this place!', CURRENT_TIMESTAMP, 15, 1, 5, 5),
+    ('The topic is very relevant in today''s world.', CURRENT_TIMESTAMP, 7, 4, 7, 5),
+    ('I enjoyed reading this article! Well done.', CURRENT_TIMESTAMP, 2, 0, 8, 5),
+    ('The author has done a fantastic job!', CURRENT_TIMESTAMP, 6, 1, 9, 6),
+    ('I didn''t find this article very helpful.',CURRENT_TIMESTAMP, 4, 12, 10, 6);
 
 
 INSERT INTO topic (name) VALUES
@@ -58,16 +61,16 @@ VALUES
 
 INSERT INTO article (name, description, date, topic_id, user_id)
 VALUES
-    ('Article 1', 'Description for Article 1', '2023-10-21 10:00:00', 1, 1),
-    ('Article 2', 'Description for Article 2', '2023-10-21 11:00:00', 2, 2),
-    ('Article 3', 'Description for Article 3', '2023-10-21 12:00:00', 1, 3),
-    ('Article 4', 'Description for Article 4', '2023-10-21 13:00:00', 2, 4),
-    ('Article 5', 'Description for Article 5', '2023-10-21 14:00:00', 1, 5),
-    ('Article 6', 'Description for Article 6', '2023-10-21 15:00:00', 2, 6),
-    ('Article 7', 'Description for Article 7', '2023-10-21 16:00:00', 1, 7),
-    ('Article 8', 'Description for Article 8', '2023-10-21 17:00:00', 2, 8),
-    ('Article 9', 'Description for Article 9', '2023-10-21 18:00:00', 1, 9),
-    ('Article 10','Description for Article 10','2023-10-21 19:00:00', 2, 10);
+    ('Tech News', 'Latest tech updates', '2023-10-21 10:00:00', 1, 1),
+    ('Science Breakthrough', 'Exciting scientific discoveries', '2023-10-21 11:00:00', 2, 2),
+    ('Sports Highlights', 'Recap of the week''s sports events', '2023-10-21 12:00:00', 3, 3),
+    ('Music Reviews', 'Latest album reviews', '2023-10-21 13:00:00', 4, 4),
+    ('Travel Destinations', 'Explore the world', '2023-10-21 14:00:00', 5, 5),
+    ('The Future of AI', 'Exploring the impact of artificial intelligence', '2023-10-21 15:00:00', 1, 6),
+    ('Football Match Analysis', 'In-depth review of the latest game', '2023-10-21 16:00:00', 3, 7),
+    ('Hidden Gems of Europe', 'Exploring lesser-known travel destinations', '2023-10-21 17:00:00', 5, 8),
+    ('Cybersecurity Best Practices', 'Protecting your online identity', '2023-10-21 18:00:00', 1, 9),
+    ('Upcoming Album Releases', 'Anticipating new music releases','2023-10-21 19:00:00', 4, 10);
 
 INSERT INTO article_vote (like, article_id, user_id)
 VALUES
@@ -97,9 +100,9 @@ VALUES
     ('2023-10-18 17:45:00', TRUE, 4);
 
 INSERT INTO notification (message) VALUES
-    ('Notification 1'),
-    ('Notification 2'),
-    ('Notification 3');
+    ('New comment on your article: "The Future of AI"'),
+    ('User Alice liked your comment on "Science Breakthrough"'),
+    ('User Bob started following you'),
 
 INSERT INTO comment_notification (notification_id, comment_id) VALUES
     (1, 1),  
@@ -144,14 +147,15 @@ INSERT INTO dislike_comment (notification_id, user_id) VALUES
     (5, 2),
     (6, 3);
 
-INSERT INTO report (description, date) VALUES
-    ('Report 1 Description', '2023-10-21 10:00:00'),
-    ('Report 2 Description', '2023-10-22 11:30:00'),
-    ('Report 3 Description', '2023-10-23 15:45:00'),
-    ('Report 4 Description', '2023-10-24 10:00:00'),
-    ('Report 5 Description', '2023-10-25 11:30:00'),
-    ('Report 6 Description', '2023-10-26 15:45:00');
 
+INSERT INTO report (description, date) VALUES
+    ('Data security breach incident', '2023-10-22 11:15:00'),
+    ('Quality control audit report', '2023-10-23 14:45:00'),
+    ('Customer service feedback analysis', '2023-10-24 09:30:00'),
+    ('Website performance evaluation', '2023-10-25 15:00:00'),
+    ('Product defects analysis', '2023-10-26 14:30:00'),
+    ('Financial expense review', '2023-10-27 10:45:00');
+    
 INSERT INTO comment_report (comment_id, report_id) VALUES
     (1, 1),
     (2, 2),
