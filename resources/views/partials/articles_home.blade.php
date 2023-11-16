17a23880a7b65c0a56b7de11e8cfdff033dac2b8
Goncalo Santos <up202108839@up.pt>
2023-11-16 11:44:22 +0000

    positioned articles

--- resources/views/partials/articles_home.blade.php
+++ resources/views/partials/articles_home.blade.php
@@ -1,44 +1,105 @@
-
 @section('content')
 
-    <section id="articlebox" class="grid-container">
+    <section id="articlebox">
         <!-- Main Article Box -->
-        <div class="article-box">
-            <div class="article-image">
-                <img src="{{ asset('images/teste.jpg') }}" alt="Article Image">
+        <div class="boxes-container">
+            <div class="article-box">
+                <div class="article-image">
+                    <img src="{{ asset('images/teste.jpg') }}" alt="Article Image">
+                </div>
+                <div class="article-content">
+                    <h1>Cristiano Ronaldo to FC Porto, here we go!</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="#" class="button">Read More</a>
+                </div>
             </div>
-            <div class="article-content">
-                <h1>Cristiano Ronaldo to FC Porto, here we go!</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                <a href="#" class="button">Read More</a>
+
+            <div class="additional-box-container">
+                <!-- First Column -->
+                <div class="column">
+                    <div class="small-box">
+                        <h2>Additional Small Box 1 Title</h2>
+                        <p>Additional Small Box 1 Content</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+
+                    <div class="small-box">
+                        <h2>Additional Small Box 2 Title</h2>
+                        <p>Bme vindo a vila moleza, o Porto aqui vai jogar e vai ganhar</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+
+                    <div class="small-box">
+                        <h2>Additional Small Box 3 Title</h2>
+                        <p>Additional Small Box 3 Content</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+                    <div class="small-box">
+                        <h2>Additional Small Box 3 Title</h2>
+                        <p>Additional Small Box 3 Content</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+                </div>
+
+                <!-- Second Column -->
+                <div class="column">
+                    <div class="small-box">
+                        <h2>Additional Small Box 4 Title</h2>
+                        <p>Additional Small Box 4 Content</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+
+                    <div class="small-box">
+                        <h2>Additional Small Box 5 Title</h2>
+                        <p>Bme vindo a vila moleza, o Porto aqui vai jogar e vai ganhar</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+
+                    <div class="small-box">
+                        <h2>Additional Small Box 6 Title</h2>
+                        <p>Additional Small Box 6 Content</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+
+                    <div class="small-box">
+                        <h2>Additional Small Box 6 Title</h2>
+                        <p>Additional Small Box 6 Content</p>
+                        <a href="#" class="small-button">Read More</a>
+                    </div>
+                </div>
             </div>
         </div>
-
-        <div class="small-box-container">
-            <div class="small-box">
-                <h2>Small Box 1 Title</h2>
-                <p>Bem vindos a vila moleza, o lugar onde vais querer estar</p>
-                <a href="#" class="small-button">Read More</a>
-            </div>
-
-            <div class="small-box">
-                <h2>Small Box 2 Title</h2>
-                <p>Small Box 2 Content</p>
-                <a href="#" class="small-button">Read More</a>
-            </div>
-
-            <div class="small-box">
-                <h2>Small Box 3 Title</h2>
-                <p>Small Box 3 Content</p>
-                <a href="#" class="small-button">Read More</a>
-            </div>
-
-            <div class="small-box">
-                <h2>Small Box 4 Title</h2>
-                <p>Small Box 4 Content</p>
-                <a href="#" class="small-button">Read More</a>
-            </div>
         </div>
+
+       
+            <div class="small-box-container">
+                <div class="small-box">
+                    <h2>Small Box 1 Title</h2>
+                    <p>Bem vindos a vila moleza, o lugar onde vais querer estar</p>
+                    <a href="#" class="small-button">Read More</a>
+                </div>
+
+                <div class="small-box">
+                    <h2>Small Box 2 Title</h2>
+                    <p>Small Box 2 Content</p>
+                    <a href="#" class="small-button">Read More</a>
+                </div>
+
+                <div class="small-box">
+                    <h2>Small Box 3 Title</h2>
+                    <p>Small Box 3 Content</p>
+                    <a href="#" class="small-button">Read More</a>
+                </div>
+
+                <div class="small-box">
+                    <h2>Small Box 4 Title</h2>
+                    <p>Small Box 4 Content</p>
+                    <a href="#" class="small-button">Read More</a>
+                </div>
+            </div>
+
+            
+        
 
     </section>
 
