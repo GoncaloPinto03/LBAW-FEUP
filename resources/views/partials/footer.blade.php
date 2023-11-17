1c3408006ebc9b303fd2e375d6336f1c125d08e9
Goncalo Santos <up202108839@up.pt>
2023-11-17 17:25:42 +0000

    finished footer (??)

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,8 +5,8 @@
             <img src="{{ asset('images/logo_big.png') }}" alt="CollabNews Logo" id="header-logo" style="width:100px;"> 
         </a>
         <div id="footer-text">
-        <a href="#">About Us</a>
-            <p>LBAW Copyright</p>
+            <a href="#">About Us</a>
+            <p><i class="fa-regular fa-copyright" style="margin: 2px;"></i>LBAW Copyright</p>
         </div>
         
     </footer>
