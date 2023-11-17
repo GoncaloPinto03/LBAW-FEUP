de86aba7f230b87139e45138e2f1ff069ed2974d
joao12520 <joaotomas12520@gmail.com>
2023-11-17 23:09:31 +0000

    Comment section added

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -28,104 +28,44 @@
         <!-- Main Article Box -->
         <div class="boxes-container">
             <div class="article-box">
-                <div class="article-image">
-                    <img src="{{ asset('images/papai.jpg') }}" alt="Article Image">
-                </div>
                 <div class="article-content">
                     <h1>Messi o PIOR do mundo, GOAT PAPAI CRIS</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
                     <a href="#" class="button">Read More</a>
                 </div>
+
+                <div class="article-image">
+                    <img src="{{ asset('images/papai.jpg') }}" alt="Article Image">
+                </div>
+
+                <div class="comments-section">
+                <h2>Comments</h2>
+                <!-- Aqui falta atualizar pagina dps do novo comment ser inserido -->
+                <form action="{}" method="post">
+                    @csrf
+                    <label for="comment">Leave a comment:</label>
+                    <textarea name="comment" id="comment" cols="30" rows="5"></textarea>
+                    <button type="submit">Submit Comment</button>
+                </form>
+
+                <ul class="comment-list">
+                    <li>
+                        <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                        <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                        <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                        <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                    <li>
+                        <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </li>
+                </ul>
             </div>
-
-            <div class="additional-box-container">
-                <!-- First Column -->
-                <div class="column">
-                    <div class="small-box">
-                        <h2>Additional Small Box 1 Title</h2>
-                        <p>Additional Small Box 1 Content</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-
-                    <div class="small-box">
-                        <h2>Additional Small Box 2 Title</h2>
-                        <p>Bme vindo a vila moleza, o Porto aqui vai jogar e vai ganhar</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-
-                    <div class="small-box">
-                        <h2>Additional Small Box 3 Title</h2>
-                        <p>Additional Small Box 3 Content</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-                    
-                    <div class="small-box">
-                        <h2>Additional Small Box 3 Title</h2>
-                        <p>Additional Small Box 3 Content</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-                </div>
-
-                <!-- Second Column -->
-                <div class="column">
-                    <div class="small-box">
-                        <h2>Additional Small Box 4 Title</h2>
-                        <p>Additional Small Box 4 Content</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-
-                    <div class="small-box">
-                        <h2>Additional Small Box 5 Title</h2>
-                        <p>Bme vindo a vila moleza, o Porto aqui vai jogar e vai ganhar</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-
-                    <div class="small-box">
-                        <h2>Additional Small Box 6 Title</h2>
-                        <p>Additional Small Box 6 Content</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-
-                    <div class="small-box">
-                        <h2>Additional Small Box 6 Title</h2>
-                        <p>Additional Small Box 6 Content</p>
-                        <a href="#" class="small-button">Read More</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-
-       
-            <div class="small-box-container">
-                <div class="small-box">
-                    <h2>Small Box 1 Title</h2>
-                    <p>Bem vindos a vila moleza, o lugar onde vais querer estar</p>
-                    <a href="#" class="small-button">Read More</a>
-                </div>
-
-                <div class="small-box">
-                    <h2>Small Box 2 Title</h2>
-                    <p>Small Box 2 Content</p>
-                    <a href="#" class="small-button">Read More</a>
-                </div>
-
-                <div class="small-box">
-                    <h2>Small Box 3 Title</h2>
-                    <p>Small Box 3 Content</p>
-                    <a href="#" class="small-button">Read More</a>
-                </div>
-
-                <div class="small-box">
-                    <h2>Small Box 4 Title</h2>
-                    <p>Small Box 4 Content</p>
-                    <a href="#" class="small-button">Read More</a>
-                </div>
-                <div class="small-box">
-                    <h2>Small Box 4 Title</h2>
-                    <p>Small Box 4 Content</p>
-                    <a href="#" class="small-button">Read More</a>
-                </div>
             </div>
 
     </section>
