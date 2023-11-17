9735268fa82cd60c854cbef6561514f8852ff143
Ricardo Sousa <ricardofegosousa@gmail.com>
2023-11-17 00:13:55 +0000

    added html for profile page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,6 +2,16 @@
 @include('partials.topbar')
 
 @section('content')
-    <h1>This is user profile</h1>
-    <h2>{{$user }}</h2>
+    <h1>User Profile</h1>
+    <section id='profile'>
+        <div class="profile-pic">
+            <img src="{{ asset('images/default_pic.jpg')}}">
+        </div>
+        <h2><strong>{{ $user->name }}</strong><h2>
+        <p>Email: {{ $user->email }}<p>
+        <p>Reputation: {{ $user->reputation }}<p>
+    </section>
+    @if (Auth::user()->user_id == $user->user_id)
+        <a href="{{ url('/home') }}" class="button">Edit Profile</a>
+    @endif
 @endsection
