1d0c9f442a07000c078753ed831c63080029bbec
joao12520 <joaotomas12520@gmail.com>
2023-11-18 16:04:26 +0000

    Topics aligned

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -27,8 +27,8 @@
                 <div class="article-content">
                     <h1>Messi o PIOR do mundo, GOAT PAPAI CRIS</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <button class="share-button">Share</button>
                 </div>
-
                 <div class="article-image">
                     <img src="{{ asset('images/papai.jpg') }}" alt="Article Image">
                 </div>
@@ -56,7 +56,7 @@
                         <li>
                             <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         </li>
-                        <li>
+                        <li>    
                             <strong>Username:</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         </li>
                     </ul>
@@ -66,11 +66,18 @@
             <div class="popular-news-section">
                 <h2>Most Popular News </h2>
                     <ul class="topic-list">
-                            <li><a href="#">Topic 1</a></li>
-                            <li><a href="#">Topic 2</a></li>
-                            <li><a href="#">Topic 3</a></li>
-                            <li><a href="#">Topic 4</a></li>
-                            <li><a href="#">Topic 5</a></li>
+                            <li class="topic-item">
+                                <a href="#">
+                                    <div class="topic-image">
+                                        <img src="{{ asset('images/teste.jpg') }}" alt="Article Image">
+                                        <div class="image-text">Lorem ipsum dolor</div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
+                            <li><a href="#">Lorem ipsum dolor</a></li>
                     </ul>
             </div>
 
