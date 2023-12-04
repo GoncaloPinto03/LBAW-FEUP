612303ca48587c339182808642b1467d928a3e06
Goncalo Santos <up202108839@up.pt>
2023-12-04 13:15:08 +0000

    small changes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,8 +1,11 @@
 @section('sidebar')
     <div id="sidebar">
         <h1>Topics</h1>
-        @foreach($topics as $topic)
-            <a href="#"><i class="fa-solid fa-chevron-right" style="color: #00003e; margin: 2px;"></i>{{ $topic->name }}</a>
-        @endforeach
+        <ul style="list-style=none;">
+            @foreach($topics as $topic)
+                <li><button id="topics"><i class="fa-solid fa-chevron-right" style="color: #00003e; margin: 2px;"></i>{{ $topic->name }}</button></li>
+            @endforeach
+        </ul>
+        
     </div>
 @endsection
