3dd43b3df9a79223c58503cd94e76b9cce6e26cd
joao12520 <joaotomas12520@gmail.com>
2023-12-19 14:58:50 +0000

    Page tag_articles created

--- resources/views/tag_articles.blade.php
+++ resources/views/tag_articles.blade.php
@@ -6,21 +6,42 @@
         <h1>Artigos com a Tag: {{ $tag->name }}</h1>
 
         @if ($articles->count() > 0)
-            <ul>
-                @foreach ($articles as $article)
-                    <li>
-                        <a href="{{ url('articles/' . $article->article_id) }}">
-                            {{ $article->name }}
-                        </a>
-                    </li>
-                @endforeach
-            </ul>
+            <div class="additional-box-container">
+                
+                <!-- First Column -->
+                <div class="column">
+                    @php
+                        $column1 = $articles->take($articles->count() / 2);
+                    @endphp
+
+                    @foreach ($column1 as $article)
+                        <div class="small-box" data-category="{{ $article->category }}">
+                            <h2>{{ $article->name }}</h2>
+                            <p>{{ $article->description }}</p>
+                            <a href="{{ url('articles/' . $article->article_id) }}" class="small-button">Read More</a>
+                        </div>
+                    @endforeach
+                </div>
+
+                <!-- Second Column -->
+                <div class="column">
+                    @php
+                        $column2 = $articles->slice($articles->count() / 2);
+                    @endphp
+
+                    @foreach ($column2 as $article)
+                        <div class="small-box" data-category="{{ $article->category }}">
+                            <h2>{{ $article->name }}</h2>
+                            <p>{{ $article->description }}</p>
+                            <a href="{{ url('articles/' . $article->article_id) }}" class="small-button">Read More</a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
         @else
             <p>Não há artigos com esta tag.</p>
         @endif
     </section>
-
-    <!-- Outras seções, rodapé, etc. -->
 
 @include('partials.footer')
 
