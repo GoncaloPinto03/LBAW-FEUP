2a4cc136e04ac5ca5f18802428a445824cd2e693
Ricardo Sousa <ricardofegosousa@gmail.com>
2023-12-21 20:24:23 +0000

    deleted user policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Models\Article' => 'App\Policies\ArticlePolicy',
-        'App\Models\User' => 'App\Policies\UserPolicy',
     ];
 
     /**
